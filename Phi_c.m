b593dc161e901128c0835836eab3a174b554915c
maki <m-kawano@ee.tokushima-u.ac.jp>
2015-08-24 21:30:52 +0900

    バンド中心のX座標による角度の変化を追加

--- Phi_c.m
+++ Phi_c.m
@@ -1,7 +1,12 @@
 function Pc=Phi_c(Theta, RadiusCylinder, R)
-% ∠OcOL
+% yz平面とLの角度
 % 入力：Theta＝バンドの中心角, RadiusCylinder＝バンド半径, R＝原点からバンド中心までの距離
-% 出力：∠OcOL
+% 出力：yz平面とLの角度
 
-Pc=acos((R.^2+OL(Theta, RadiusCylinder, R).^2-RadiusCylinder.^2)./(2.*R.*OL(Theta, RadiusCylinder, R)));
+Ocx=-10;                % 脱分極バンド中心のX座標
+OcAngle=atan(Ocx/R);    % yz平面とOcの角度
+
+% ∠OcOL
+OLAngle=acos((R.^2+OL(Theta, RadiusCylinder, R).^2-RadiusCylinder.^2)./(2.*R.*OL(Theta, RadiusCylinder, R)));
+Pc=OLAngle+OcAngle;
 end
